9a6a504277ac4976fe9f3d6e192d23298a9cf931
ayanrv <nevvidoma@gmail.com>
2025-12-05 02:54:48 +0100

    Build: generate docs for GitHub Pages

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,4 +1,17 @@
+/* eslint-disable prettier/prettier */
+const path = require('path');
+
 module.exports = {
-	mode: 'production',
-	devtool: false,
+  mode: 'production',
+  devtool: false,
+  output: {
+    // 👉 собираем прямо в корневую папку docs
+    path: path.resolve(__dirname, '../docs'),
+    filename: 'static/scripts/[name].[contenthash].js',
+
+    // 👉 ОЧЕНЬ важно: относительные пути, чтобы работало на /article-styler/
+    publicPath: './',
+  },
 };
+
+/* eslint-enable prettier/prettier */
